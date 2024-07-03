5c2aabbd41c56bafe21082e9178598b955ad70dc
tejaswinigowda26 <tejukrishna2006@gmail.com>
2024-07-03 11:55:36 +0530

    Update one.java

--- one.java
+++ one.java
@@ -2,7 +2,7 @@ public class hello
 {
 	public static void main(String args[])
 	{
-		System.out.println("hello world")
-			System.out.println("fs")
+		System.out.println("hello world");
+			System.out.println("fs,tumkuru");
 	}
 }
